0d9f9002fc2aa560c3503426f277cc06b0cb6d4a
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-11-01 15:18:54 +0900

    debug = True

--- goldenroof_project/settings.py
+++ goldenroof_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY", "4xv&kr+!u(5qv6x$(5svf-@g8lhv!ra4(5&rkhu5l0b00qaal+")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ["goldenroof.herokuapp.com", "127.0.0.1"]
