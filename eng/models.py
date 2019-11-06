786aa3e48976d33fd78d081a084a63b295cadeb3
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-11-06 17:29:48 +0900

    Articles models added WIP

---
+++ eng/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
+
+
+class Keyword(models.Model):
+    name = models.CharField("Keyword", max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField("Category", max_length=20)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(max_length=280, default=settings.SITE_DESCRIPTION) #SEO head meta tag Description for a category page
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("eng:category", kwargs={"slug": self.slug})
+
+    class Meta:
+        verbose_name = "Category"
+        ordering = ["name"]
+
+
+class Article(models.Model):
+    IMG_LINK = "static/cover_image_plaseholder.jpg"
+    title = models.CharField(verbose_name="Title", max_length=255)
+    author = models.ForeignKey(User, verbose_name="Author", on_delete=models.CASCADE)
+    description = models.CharField(verbose_name="Description", max_length=280)
+    content = models.TextField(verbose_name="Content")
+    img_link = models.CharField(verbose_name="Cover image", default=IMG_LINK, max_length=255)
+    created_on = models.DateTimeField(verbose_name="Created", auto_now_add=True)
+    updated_on = models.DateTimeField(verbose_name="Updated", auto_now=True)
+    views = models.IntegerField(verbose_name="Views", default=0)
+    slug = models.SlugField(unique=True)
+    is_published = models.BooleanField("Published", default=False)
+
+    category = models.ForeignKey(Category, verbose_name="Category", on_delete=models.PROTECT)
+    keywords = models.ManyToManyField(Keyword, verbose_name="Keywords")
+
+
+    class Meta:
+        verbose_name = "Article"
+        ordering = ["-created_on"]
+
+    
+    def __str__(self):
+        return self.title[:20]
+
+    
+    def get_absolute_url(self):
+        return reverse("eng:article_detail", kwargs={"slug": self.slug})
+
+
+    def update_views(self):
+        self.views += 1
+        self.save(update_fields=["views"])
