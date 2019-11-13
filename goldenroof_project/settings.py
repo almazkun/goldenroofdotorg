ecf044c7449d4608ac7ca07a6546e5c7fb4f6013
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-11-13 12:55:06 +0900

    managing local module

--- goldenroof_project/settings.py
+++ goldenroof_project/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import dj_database_url
 
-from secret import LOCAL_EMAIL_USER, LOCAL_EMAIL_PASSWORD, LOCAL_DEFAULT_FROM_EMAIL
+try:
+    from secret import LOCAL_EMAIL_USER, LOCAL_EMAIL_PASSWORD, LOCAL_DEFAULT_FROM_EMAIL
+except:
+    pass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
