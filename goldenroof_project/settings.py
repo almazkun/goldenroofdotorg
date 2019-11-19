3362c1a734324886e3b5a8875ac8a475ec69b25d
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-11-19 18:04:44 +0900

    undockerize, nav bar

--- goldenroof_project/settings.py
+++ goldenroof_project/settings.py
@@ -89,6 +89,14 @@ WSGI_APPLICATION = "goldenroof_project.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+"""
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "super_db",
@@ -98,7 +106,7 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-
+"""
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
 
